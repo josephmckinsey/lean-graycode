95793863640e5efab6388380517f4c9d8da90a2c
Joseph Mckinsey <joseph.mckinsey@digitalgeospecialists.com>
2025-09-08 17:20:08 -0600

    Refactor list stability, use Nat.log2 in binary rec, and finish list_is_recursive

--- Graycode/Basic.lean
+++ Graycode/Basic.lean
@@ -1,4 +1,4 @@
---import Mathlib
+import Mathlib
 import Mathlib.Algebra.Ring.GeomSum
 import Mathlib.Data.Nat.Bitwise
 import Mathlib.Analysis.Normed.Ring.Lemmas  -- I have no idea why this is necessary
@@ -169,7 +169,7 @@ def IsGrayCode {α : Type*} [AddGroupWithOne α] (x : α → ℕ) : Prop :=
 
 def list_gray_code : ℕ → List ℕ
 | 0 => [0]
-| .succ n => (list_gray_code n).append ((list_gray_code n).reverse.map (2^n ^^^ ·))
+| .succ n => (list_gray_code n) ++ ((list_gray_code n).reverse.map (2^n ^^^ ·))
 
 
 def is_list_gray_code (x : List ℕ) : Bool := Id.run do
@@ -200,11 +200,11 @@ lemma list_gray_code_length (n : ℕ) :
 lemma list_is_stable_once (n : ℕ) (i : ℕ) (h : i < 2 ^ n) :
   (list_gray_code n)[i]'(by simp [h]) = (list_gray_code (n+1))[i]'(by grind) := by
   have := list_gray_code.eq_2 n
-  simp only [Nat.succ_eq_add_one, List.map_reverse, List.append_eq] at this
+  simp only [Nat.succ_eq_add_one, List.map_reverse] at this
   simp_rw [this]
   rw [List.getElem_append_left']
 
-lemma list_is_stable (n : ℕ) (i : ℕ) (h : i < 2 ^ n) (m : ℕ) (h' : n ≤ m) :
+lemma list_is_stable' (n : ℕ) (i : ℕ) (h : i < 2 ^ n) (m : ℕ) (h' : n ≤ m) :
   (list_gray_code n)[i]'(by simp [h]) = (list_gray_code m)[i]'(by
     rw [list_gray_code_length]
     suffices 2^n ≤ 2^m by linarith
@@ -217,6 +217,137 @@ lemma list_is_stable (n : ℕ) (i : ℕ) (h : i < 2 ^ n) (m : ℕ) (h' : n ≤ m
     apply list_is_stable_once
     suffices 2^n ≤ 2^k by linarith
     apply Nat.pow_le_pow_right (by simp) (by assumption)
+
+lemma list_is_stable (n : ℕ) (i : ℕ) {h : i < (list_gray_code n).length} (m : ℕ) (h' : i < 2 ^ m) :
+  (list_gray_code n)[i]'h = (list_gray_code m)[i]'(by
+    rw [list_gray_code_length]
+    assumption
+  ) := by
+  wlog n_le_m : n ≤ m generalizing n m
+  · have m_le_n : m ≤ n := Nat.le_of_not_ge n_le_m
+    exact Eq.symm (list_is_stable' m i h' n m_le_n)
+  rw [list_is_stable']
+  · simp_all
+  exact n_le_m
+
+lemma complement_is_smaller (i n : ℕ) (h : 2 ^ n ≤ i ∧ i < 2 ^ (n + 1)) :
+  2 ^ (n + 1) - 1 ^^^ i < 2^n := by
+  apply Nat.lt_pow_two_of_testBit
+  intro j jh
+  rcases lt_or_eq_of_le jh with h' | h'
+  · simp only [Nat.testBit_xor, Nat.testBit_two_pow_sub_one]
+    rw [decide_eq_false (by grind)]
+    simp only [Bool.false_bne]
+    apply Nat.testBit_eq_false_of_lt
+    apply lt_of_lt_of_le h.2
+    apply Nat.pow_le_pow_right (by norm_num) (by assumption)
+  have : i.testBit n = true := by
+    exact order_two_pow_testBit_true i n h.1 h.2
+  simp [<-h', this]
+
+lemma Nat.log2_eq (i n : ℕ) (nezero : i ≠ 0) :
+  2^n ≤ i ∧ i < 2^(n+1) ↔ i.log2 = n:= by
+  rw [Nat.log2_eq_log_two, Nat.log_eq_iff]
+  simp [nezero]
+
+universe u in
+@[elab_as_elim, specialize]
+def binaryComplementRec {motive : Nat → Sort u} (zero : motive 0)
+  (two_pow : ∀ i, i > 0 → motive ((2 ^ (i.log2 + 1) - 1) ^^^ i) → motive i)
+    (n : Nat) : motive n :=
+  if n0 : n = 0 then congrArg motive n0 ▸ zero
+  else
+    let descent := binaryComplementRec zero two_pow ((2^(n.log2+1) - 1) ^^^ n)
+    two_pow n (Nat.zero_lt_of_ne_zero n0) descent
+decreasing_by
+  have := Nat.log2_self_le n0
+  linarith [complement_is_smaller n n.log2 ((Nat.log2_eq n n.log2 n0).mpr rfl)]
+
+def recursive_gray_code (n : ℕ) : ℕ :=
+  binaryComplementRec
+    (zero := 0) -- zero case
+    (two_pow := fun i _ complement ↦ 2^(i.log2) ^^^ complement)
+      -- do when you have the complement
+    n
+
+def recursive_gray_code_eq {i : ℕ} (nezero : i ≠ 0) :
+  recursive_gray_code i = 2^(i.log2) ^^^ recursive_gray_code ((2^(i.log2+1) - 1) ^^^ i) := by
+  rw [recursive_gray_code, binaryComplementRec]
+  simp_rw [dite_eq_ite, if_neg nezero]
+  rw [<-recursive_gray_code]
+
+/--
+info: true
+-/
+#guard_msgs in
+#eval (List.range (2^5)).all (fun i ↦ recursive_gray_code i == (list_gray_code 5)[i]!)
+
+lemma Nat.two_pow_xor_eq {x n : ℕ} (h : x < 2 ^ n) :
+  2 ^ n - 1 ^^^ x = 2 ^ n - 1 - x := by
+  rw [show 2^n - 1 - x = 2^n - (x + 1) by omega]
+  apply Nat.eq_of_testBit_eq
+  intro i
+  rw [Nat.testBit_two_pow_sub_succ h]
+  simp only [testBit_xor, testBit_two_pow_sub_one]
+  have : x.testBit i → decide (i < n) := by
+    simp only [decide_eq_true_eq]
+    intro h'
+    contrapose! h'
+    simp only [Bool.not_eq_true]
+    apply Nat.testBit_eq_false_of_lt
+    have := Nat.pow_le_pow_right (n := 2) (by norm_num) h'
+    linarith
+  grind
+
+
+@[reducible]
+def list_gray_code_i (i : ℕ) :=
+  (list_gray_code (i.log2 + 1))[i]'(by
+    rcases Nat.eq_zero_or_pos i with rfl | h
+    · simp
+    simp [Nat.lt_log2_self]
+  )
+
+
+lemma list_is_recursive_aux (i : ℕ) :
+  list_gray_code_i i = recursive_gray_code i := by
+  induction i using binaryComplementRec with
+  | zero => simp [list_gray_code_i, recursive_gray_code, list_gray_code, binaryComplementRec]
+  | two_pow i ih complement =>
+    rw [list_gray_code_i] at *
+    simp_rw [list_gray_code]
+    rw [List.getElem_append_right (by simp [Nat.log2_self_le (Nat.ne_zero_of_lt ih)])]
+    rw [List.getElem_map, List.getElem_reverse]
+    simp only [list_gray_code_length]
+    have : (2^i.log2 - 1 - (i - 2^i.log2)) = (2^(i.log2+1) - 1) ^^^ i := by
+      -- I hope I don't need thisone again
+      have := Nat.log2_self_le (Nat.ne_zero_of_lt ih)
+      rw [show 2^i.log2 - 1 - (i - 2^i.log2) = 2^(i.log2 + 1) - 1 - i by omega]
+      exact (Nat.two_pow_xor_eq Nat.lt_log2_self).symm
+    simp_rw [this]
+    have smaller_complement:= complement_is_smaller i (i.log2)
+      ((Nat.log2_eq i i.log2 (Nat.ne_zero_of_lt ih)).mpr rfl)
+    rw [list_is_stable (n := ((2 ^ (i.log2 + 1) - 1 ^^^ i).log2 + 1))
+      (i := 2 ^ (i.log2 + 1) - 1 ^^^ i)
+      (m := i.log2) smaller_complement] at complement
+    rw [complement]
+    rw [<-recursive_gray_code_eq]
+    exact Nat.ne_zero_of_lt ih
+
+@[simp]
+lemma list_gray_code_zero (n : ℕ) :
+  (list_gray_code n)[0] = 0 := by
+  induction n with
+  | zero => simp [list_gray_code]
+  | succ n nh =>
+    unfold list_gray_code
+    simp [nh]
+
+lemma list_is_recursive (i n : ℕ) {h : i < (list_gray_code n).length} :
+  (list_gray_code n)[i] = recursive_gray_code i := by
+  rw [list_is_stable (n := n) (i := i) (m := i.log2 + 1)]
+  · exact list_is_recursive_aux i
+  exact Nat.lt_log2_self
 
 
 def hello := "world"
